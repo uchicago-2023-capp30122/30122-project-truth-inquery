92be81800a82b6464dd67416783b517d5d9dd971
deetherese <dema@uchicago.edu>
2023-02-16 15:08:33 -0600

    Add file on API requests

--- database_model/API_data/api_requests.py
+++ database_model/API_data/api_requests.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 from urllib.parse import urlparse
 
-apikey = '############'
+apikey = '########'
 
 def make_link_absolute(rel_url, current_url):
     """
--- database_model/database.py
+++ database_model/database.py
@@ -1,24 +1,38 @@
 import glob
-import sqlite3
 import pandas as pd
+# import sqlite3
 
 path1 = "./database_model/API_data"
-path2 = "./truth_inquery/output"
+path2 = "./truth_inquery/output" 
 path3 = "./truth_inquery/data"
 
-def concat_files(path): #helper function 
+def concat_files(path, index): #helper
     """
     """
     files = glob.glob(path + "/*.csv")
-    dataframe = pd.DataFrame()
+    # dataframe = pd.DataFrame()
     content = []
     for filename in files:
-        df = pd.read_csv(filename, index_col = None)
+        df = pd.read_csv(filename, index_col = index)
         content.append(df)
     data_frame = pd.concat(content)
     return data_frame
 
-clinics = concat_files(path3)
+clinics = concat_files(path3, 3)
+API_data = concat_files(path1, 0)
+API_data
+
+# files = glob.glob(path2 + "/*.csv")
+# files
+# conn = sqlite3.connect("token_states.db") 
+# for file_name in files:
+#     table_name = file_name.split('.')[0]
+#     df = pd.read_csv(file_name)
+#     df.to_sql(table_name, conn, if_exists='append', index=False)
+# conn.close()
+
+
+
 
 # # connection = sqlite3.connect("AlabamaTokens.db")
 # # df.to_sql("token_AL", connection, if_exists = "replace")
