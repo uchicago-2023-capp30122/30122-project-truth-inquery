05cf2edadaeb02fe1256fce7d266ab546fb77ecb
Aaron Haefner <aaronhaefner@uchicago.edu>
2023-02-16 22:47:29 -0600

    Tokenization update, tests, and data upload

--- truth_inquery/crawler/crawler.py
+++ truth_inquery/crawler/crawler.py
@@ -3,52 +3,21 @@ import pandas as pd
 import re
 import scrapelib
 import time
-from urllib.error import HTTPError
-from hcp_urls import get_hcp_base
+import glob
+import sys
+
 s = scrapelib.Scraper(retry_attempts=0, retry_wait_seconds=0)
 
-
-
-"""
-Change state crawl function to work as network crawl function
-that works with a list of urls as inputs (extract URLs outside of function from state files)
-- maybe generate ahref lists in separate helper function.
-
-zip codes:
-- we can visualize where the CPCs are at the state-level using maps (plotly, geo spacial data)
-- top tokens by state
-
-CPC ZIP CODE, potentially look up the zip code that is captured in the Abortion Finder website
-
-- Seaborn
-- mapping
-- work with data interactively in jupyter notebooks 
-i.e., import your two datasets in a different notebook
-"""
-
-
+PATTERN = r'[\[0-9-()="?!}{<>.,~`@#$%&*^_+:;|\]\\\/]'
 INDEX_IGNORE = (
-    "a",
-    "an",
     "and",
     "are",
-    "as",
-    "at",
-    "be",
-    "by",
     "for",
     "from",
     "has",
-    "he",
-    "in",
-    "is",
-    "it",
     "its",
-    "of",
-    "on",
     "that",
     "the",
-    "to",
     "was",
     "were",
     "will",
@@ -57,8 +26,8 @@ INDEX_IGNORE = (
 
 def clean_df(df):
     """
-        Clean pd dataframe. replace NaN with 0.
-        collapse columns into joint columns by summing key total
+    Clean pd dataframe. replace NaN with 0.
+    collapse columns into single column by summing token count Total
     """
     output = df.fillna(0)
     output['Total'] = df.sum(axis=1)
@@ -89,7 +58,7 @@ def get_root(url):
     Extracts html root from url if possible
 
     Inputs:
-        url (str): url
+        url (str): URL
 
     Returns: set of URLs otherwise None
     """
@@ -102,32 +71,36 @@ def get_root(url):
 
 def tokenize(root):
     """
-    Tokens are created by iterating over all text 
-    elements (regardless of tag - needs refinement), on a website.
+    Tokens are created by iterating over the split text contained in
+    all HTML tags on a website.
 
-    Currently: tokens are selected by excluding anything with a special character
     Inputs:
         HTML 'Root' element from a website from which text is scraped
     
-    Returns dictionary of with token-frequency key-values
-    pairs from website.
+    Returns: dictionary of token-frequency key-values pairs.
     """
     tokens = {} 
-    pattern = re.compile(r'[0-9()="?!}{<>.,~`@#$%&*^_+:;|]')
+    pattern = re.compile(PATTERN)
+
+    # Spend a maximum of 5 minutes tokenizing text
+    timeout = time.time() + 60*5
     all_text = ''.join(root.itertext())
-    timeout = time.time() + 60*5   # 5 minutes from now
+
     for key in all_text.split():
         str_key = str(key).lower()
+        # Ignore if token contains any chars in pattern
         if re.search(pattern, str_key) is not None:
             continue
-
-        if str_key not in INDEX_IGNORE and len(str_key) > 1:
+        # Word exclusion and length restrictions
+        if str_key not in INDEX_IGNORE and len(str_key) > 2 and len(str_key) < 25:
             if str_key not in tokens:
                 tokens[str_key] = 1
             else:
                 tokens[str_key] += 1
+
         if time.time() > timeout:
             return tokens
+
     return tokens
 
 def crawl(url, limit):
@@ -147,9 +120,11 @@ def crawl(url, limit):
     if root is None:
         return pd.DataFrame(dct)
 
+    # Tokenize the base URL (input)
     dct['base'] = tokenize(root)
     urls = set(root.xpath('//a/@href'))
     
+    # Tokenize up to limit # sub-URLs 
     for u in urls:
         subroot = get_root(u)
         if subroot is not None:
@@ -175,54 +150,66 @@ def network_crawl(urllst, outpath, limit=15):
 
     Returns: None, creates csv file
     """
+    # Counter
     b = 1
-    # print("Base URL", b, "crawling")
+
+    # First URL is put in DF to join with others
     df = clean_df(crawl(urllst[0], limit))
-    # print("Finished")
 
     for i, b_url in enumerate(urllst[1:]):
+        # Crawl and clean
         print("Base URL", b, "crawling")
         new_df = clean_df(crawl(b_url, limit))
+
+        # Join df and new_df
         df = df.join(new_df.add_suffix(str(i+1)), how='outer')
         df = df.fillna(0)
         print("Finished")
         b += 1
+
+    # Clinic/URL-level data
+    clinic = df.reset_index()
+    clinic = clinic.rename({'index':'token','Total':'Total0'}, axis=1)
+    clinic.to_csv(outpath.replace("_tokens","_clinics"))
+
     # Row-wise sum, one column of token counts by state
-    df = clean_df(df)
-    df = df.reset_index()
-    df.to_csv(outpath)
+    state = clean_df(df)
+    state = state.reset_index()
+    state = state.rename({'index':'token'}, axis=1)
+    state.to_csv(outpath)
     print("CSV saved")
     return df
 
 if __name__ == "__main__":
 
-    #banned: Alabama Arkansas Idaho Kentucky Louisiana Mississippi Missouri Oklahoma South Dakota Tennessee Texas
-    # datafiles = ["truth_inquery/data/Georgia (GA).csv"]
-    # datafiles = ["truth_inquery/data/Hawaii (HI).csv", "truth_inquery/data/Maryland (MD).csv", "truth_inquery/data/Michigan (MI).csv", "truth_inquery/data/Montana (MT).csv", "truth_inquery/data/Nebraska (NE).csv", "truth_inquery/data/New Mexico (NM).csv"]
-    # datafiles = ["truth_inquery/data/North Carolina (NC).csv", "truth_inquery/data/North Dakota (ND).csv", "truth_inquery/data/Ohio (OH).csv", "truth_inquery/data/Oregon (OR).csv", "truth_inquery/data/Pennsylvania (PA).csv", "truth_inquery/data/Rhode Island (RI).csv"]
-    # datafiles = ["truth_inquery/data/South Carolina (SC).csv", "truth_inquery/data/Utah (UT).csv", "truth_inquery/data/Vermont (VT).csv", "truth_inquery/data/Virginia (VA).csv", "truth_inquery/data/Washington (WA).csv", "truth_inquery/data/West Virginia (WV).csv", "truth_inquery/data/Wisconsin (WI).csv" , "truth_inquery/data/Wyoming (WY).csv"]
-    datafiles = ["truth_inquery/data/Maine (ME).csv"]
-    for file in datafiles:
+    #banned: Alabama Arkansas Idaho Kentucky Louisiana Mississippi Missouri Oklahoma South Dakota Tennessee Texas West Virginia
+    #stopped scheduling: North Dakota Wisconsin
+    # state_inputs = glob.glob("truth_inquery/data/*).csv")
+    state_inputs = ["truth_inquery/data/Ohio (OH).csv"]
+    for file in state_inputs:
         state = file[-7:]
         state = state[:2]
-
+        print("Crawling CPCs in", state)
         df = csv_extract(file)
         urls = df['url'].tolist()
         outpath = "truth_inquery/output/" + state + "_CPC_tokens.csv"
-        print("Crawling CPCs in ", state)
-        # network_crawl(urls, outpath, limit = 50)
+        
+        network_crawl(urls, outpath, limit = 50)
 
-        # Collect tokens from HCPs
-        # zips = set(df['zip'].tolist())
-        # hcp = get_hcp_base(zips, state)
-        # hcp_urls = hcp.loc['url'].to_list()
-
-        hcp = pd.read_csv("truth_inquery/data/hcp_urls_" + state + ".csv")
+    # Collect tokens from HCPs
+    states = ["MI", "MN", "OH"]
+    base = "truth_inquery/data/hcp_urls_"
+    # clinic_inputs = glob.glob("truth_inquery/data/hcp_urls*")
+    # for file in clinic_inputs:
+    for state in states:
+        file = base + state + ".csv"
+        # state = file[-6:-4]
+        print("Crawling HCPs in", state)
+        hcp = pd.read_csv(file)
         hcp_urls = hcp.iloc[0].to_list()
         
         baseurls = list(set(hcp_urls))
         outpath = "truth_inquery/output/" + state + "_HCP_tokens.csv"
-        print("Crawling HCPs in", state)
         network_crawl(baseurls, outpath, limit = 50)
 
-        print(state,"saved")
+    print(state,"CPCs saved")
--- truth_inquery/crawler/hcp_urls.py
+++ truth_inquery/crawler/hcp_urls.py
@@ -7,43 +7,46 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+import glob
+from crawler import csv_extract
 
 WAIT = 5
 START = "https://www.abortionfinder.org"
 
-def get_hcp_base(zips, state =""):
+def get_hcp_base(zips, state):
     """
-    Navigates web browser to an abortion finder search and 
+    Navigates web browser to an abortion finder search and
     enters each CPC zip code to search for nearby abortion healthcare providers (HCPs).
 
     When available, the crawler collects distance, address, and url (base URL) for the
     top search result (ordered by distance in miles, alphabetical otherwise).
+    Crawler does not require all data to be collected, hence many try/except blocks.
 
     Input:
-        - state (str): US state 2 letter abbreviation ******************************
         - zips (iterable/set): set of zip codes with at least one CPC in the state
+        - state (str): US state 2 letter abbreviation ******************************
     
     Returns:
     df (dataframe) containing data for each inital search result URL (base URL) (str) with
         - List of sub-URLS for each base (list of lists)
-        - Distance between CPC and HCP (str)
+        - Distance between CPC zip and HCP (str)
         - Address of HCP (str)
     """
     # Create webdriver client for Chrome
     driver = webdriver.Chrome()
+    actions = ActionChains(driver)
     hcp_data = {}
     k = 1
 
     # Loop through CPC zip codes
-    for z in zips:
+    for zip in zips:
         base = {}
         driver.get(START)
 
         # Enter zip code
-        driver.find_element(By.XPATH, '//*[@id="location-box"]').send_keys(z)
+        driver.find_element(By.XPATH, '//*[@id="location-box"]').send_keys(zip)
 
-        # Tab to box to fill
-        actions = ActionChains(driver)
+        # Tab checkbox and fill
         for _ in range(3):
             actions.send_keys(Keys.TAB)
             actions.perform()
@@ -55,7 +58,7 @@ def get_hcp_base(zips, state =""):
         driver.find_element(By.XPATH, '//button[@type="submit"]').click()
         time.sleep(WAIT)
 
-        # Limit to physical locations
+        # Limit to physical locations, wait one second between clicks
         try:
             driver.find_element(By.XPATH, '//button[@class="telehealth-service ribbon-item desktop-only  "]').click()
             time.sleep(WAIT//WAIT)
@@ -73,7 +76,6 @@ def get_hcp_base(zips, state =""):
             distance = ''
 
         # Try to navigate to the next page and collect url (base url)
-        # If base url can be collected, collect sub URLs (a tags with hrefs)
         try:
             # First result
             driver.find_element(By.XPATH, '//div[@class="pill-link-container"]').click()
@@ -83,10 +85,10 @@ def get_hcp_base(zips, state =""):
             base_url = ele.get_attribute("href")
             if not base_url:
                 base_url = driver.current_url
-            # print(base_url)
         except NoSuchElementException:
             continue
         
+        # Exclude if zipcode search results in abortion provider not in input state (state of CPC)
         try:
             address = driver.find_element(By.XPATH, '//section[@class="location-hours"]//p').text
             if state not in address:
@@ -94,7 +96,7 @@ def get_hcp_base(zips, state =""):
         except NoSuchElementException:
             address = ''
         
-        # Build dictionary for each HCP search result
+        # Build dictionary
         base['url'] = base_url
         base['distance'] = distance
         base['address'] = address
@@ -108,4 +110,15 @@ def get_hcp_base(zips, state =""):
     out = "truth_inquery/data/hcp_urls_" + state + ".csv"
     df.to_csv(out)
     return df
-    
+
+# Collect URLs for all states with CPC data (where abortion legal)
+if __name__ == "__main__":
+    state_inputs = glob.glob("truth_inquery/data/*).csv")
+
+    for file in state_inputs:
+        state = file[-7:]
+        state = state[:2]
+        # Collect HCP URLs
+        df = csv_extract(file)
+        zips = set(df['zip'].tolist())
+        hcp = get_hcp_base(zips, state)
