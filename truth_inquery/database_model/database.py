fe3b2e6906a9a4828304d8eb7226cac3562b3769
deetherese <114553804+deetherese@users.noreply.github.com>
2023-02-27 13:47:55 -0600

    Update database.py

--- truth_inquery/database_model/database.py
+++ truth_inquery/database_model/database.py
@@ -4,10 +4,13 @@ import os
 import pandas as pd
 import sqlite3
 
-path1 = "../database_model"
+# path1 = "../database_model" 
+path1 = "truth_inquery" 
 path2 = "../truth_inquery/output" 
 path3 = "../truth_inquery/data"
 
+
+### Implements the glob module to access all the csv files in specified pathname
 files = glob.glob(path3 + "/*.csv")
 conn = sqlite3.connect("token_states.db") 
 for file_name in files:
@@ -16,8 +19,17 @@ for file_name in files:
     df.to_sql(table_name, conn, if_exists='append', index=False)
 conn.close()
 
-def concat_files(path, index): #helper
+def concat_files(path, index): 
     """
+    A function to assist in API source database creation by first concatenating 
+    all the csv format files in a given path and then converting it to a dataframe
+    Input:
+        index -
+        A path to the folder in the directory that stores the required csv files
+        path -
+        An integer that serves as the index of the datafram
+    Returns:
+        A compiled dataframe of all the csv files
     """
     files = glob.glob(path + "/*.csv")
     dataframe = pd.DataFrame()
@@ -28,8 +40,17 @@ def concat_files(path, index): #helper
     data_frame = pd.concat(content)
     return data_frame
 
-def concat_files1(): #helper
+def concat_files1(): 
     """
+    A function to assist in CPC source database creation by first concatenating 
+    all the csv format files in a given path and then converting it to a dataframe.
+    Input:
+        index -
+        A path to the folder in the directory that stores the required csv files
+        path -
+        An integer that serves as the index of the datafram
+    Returns:
+        A compiled dataframe of all the csv files
     """
     filesCPC = []
     for fname in os.listdir(path=path3):
@@ -43,8 +64,17 @@ def concat_files1(): #helper
     data_frame = pd.concat(content)
     return data_frame
 
-def concat_files2(): #helper
+def concat_files2(): 
     """
+    A function to assist in HPC source database creation by first concatenating 
+    all the csv format files in a given path and then converting it to a dataframe.
+    Input:
+        index -
+        A path to the folder in the directory that stores the required csv files
+        path -
+        An integer that serves as the index of the datafram
+    Returns:
+        A compiled dataframe of all the csv files
     """
     filesHPC = []
     for fname in os.listdir(path=path3):
@@ -57,6 +87,13 @@ def concat_files2(): #helper
         content.append(df)
     data_frame = pd.concat(content)
     return data_frame
+
+
+
+
+
+### Implements the respective functions for API, CPC and HPC to convert them to 
+### sqlite3 databases
 
 cpc_clinics = concat_files1()
 hpc_clinics = concat_files2()
