0ab8640817231a32fb6720362a3a0633a243420f
deetherese <114553804+deetherese@users.noreply.github.com>
2023-02-27 15:36:50 -0600

    Update database.py

--- truth_inquery/database_model/database.py
+++ truth_inquery/database_model/database.py
@@ -4,20 +4,9 @@ import os
 import pandas as pd
 import sqlite3
 
-# path1 = "../database_model" 
-path1 = "truth_inquery" 
+path1 = "../truth_inquery/database_model" #API path 
 path2 = "../truth_inquery/output" 
-path3 = "../truth_inquery/data"
-
-
-### Implements the glob module to access all the csv files in specified pathname
-files = glob.glob(path3 + "/*.csv")
-conn = sqlite3.connect("token_states.db") 
-for file_name in files:
-    table_name = file_name.split('/')[-1]
-    df = pd.read_csv(file_name)
-    df.to_sql(table_name, conn, if_exists='append', index=False)
-conn.close()
+path3 = "../truth_inquery/data" 
 
 def concat_files(path, index): 
     """
@@ -40,75 +29,66 @@ def concat_files(path, index):
     data_frame = pd.concat(content)
     return data_frame
 
-def concat_files1(): 
+
+def clinic_related_db(type, title):
     """
-    A function to assist in CPC source database creation by first concatenating 
-    all the csv format files in a given path and then converting it to a dataframe.
+    A function to assist in statewise clinic level database creation by 
+    converting all the csv format files in a given path to a dataframe
     Input:
-        index -
-        A path to the folder in the directory that stores the required csv files
-        path -
-        An integer that serves as the index of the datafram
+        type -
+        A string indicating the category of clinic type i.e. CPC or HPC 
+        title -
+        A string indicating the table name for each state in the database
     Returns:
-        A compiled dataframe of all the csv files
+        A database for the category with a table for each state 
     """
+    conn = sqlite3.connect(f"{title}.db")
     filesCPC = []
-    for fname in os.listdir(path=path3):
-        if re.match("CPC_.*\.csv", fname):
-            filesCPC.append(path3 + "/" + fname)
-    dataframe = pd.DataFrame()
-    content = []
-    for filename in filesCPC:
-        df = pd.read_csv(filename, index_col = 3)
-        content.append(df)
-    data_frame = pd.concat(content)
-    return data_frame
+    for fname in os.listdir(path3):
+        if re.match(f"{type}_.*\.csv", fname):
+            filesCPC.append(path3 + "/" + fname) 
+    for file_name in filesCPC:
+        table_name = file_name.split('_')[-1].split(".")[0]
+        df = pd.read_csv(file_name)
+        df.to_sql(table_name, conn, if_exists='append', index=False)
+    conn.close()
 
-def concat_files2(): 
+
+def token_related_db(type, title):
     """
-    A function to assist in HPC source database creation by first concatenating 
-    all the csv format files in a given path and then converting it to a dataframe.
+    A function to assist in tokens of clinic categories database creation by 
+    converting all the csv format files in a given path to a dataframe
     Input:
-        index -
-        A path to the folder in the directory that stores the required csv files
-        path -
-        An integer that serves as the index of the datafram
+        type -
+        A string indicating the category of clinic type i.e. CPC or HPC 
+        title -
+        A string indicating the table name for each state in the database
     Returns:
-        A compiled dataframe of all the csv files
+        A database for the category with a table for each state 
     """
-    filesHPC = []
-    for fname in os.listdir(path=path3):
-        if re.match("HPC_.*\.csv", fname):
-            filesHPC.append(path3 + "/" + fname)
-    dataframe = pd.DataFrame()
-    content = []
-    for filename in filesHPC:
-        df = pd.read_csv(filename)
-        content.append(df)
-    data_frame = pd.concat(content)
-    return data_frame
+    conn1 = sqlite3.connect(f"{title}.db")
+    files_toks = []
+    for fname in os.listdir(path2):
+        if re.match(f"{type}_.*\.csv", fname):
+            files_toks.append(path2 + "/" + fname) 
+    for file_name in files_toks:
+        table_name = file_name.split('_')[-2].split(".")[0]
+        df = pd.read_csv(file_name)
+        df.to_sql(table_name, conn1, if_exists='append', index=False)
+    conn1.close()
 
-
-
-
-
-### Implements the respective functions for API, CPC and HPC to convert them to 
+### Implements/calls the respective functions for API, CPC and HPC to convert them to 
 ### sqlite3 databases
-
-cpc_clinics = concat_files1()
-hpc_clinics = concat_files2()
 API_data = concat_files(path1, 0)
-
-conn1 = sqlite3.connect("api.db")
-API_data.to_sql(name="API", con = conn1)
+conn = sqlite3.connect("api.db")
+API_data.to_sql(name="API", con = conn)
 conn.close()
 
-conn2 = sqlite3.connect("CPC_clinics.db")
-cpc_clinics.to_sql(name="CPC_clinics", con = conn2)
-conn.close()
+CPC_clinics = clinic_related_db("CPC", "CPC_clinics")
+HPC_clinics = clinic_related_db("HPC", "HPC_clinics")
 
-conn3 = sqlite3.connect("HPC_clinics.db")
-hpc_clinics.to_sql(name="HPC_clinics", con = conn3)
-conn.close()
+Tokens_CPC_clinics = token_related_db("CPC", "Tokens_CPC_clinics")
+Tokens_HPC_clinics = token_related_db("HPC", "Tokens_HPC_clinics")
+
 
 
